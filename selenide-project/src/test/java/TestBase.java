263aad0a3147a62ed677847df7e01fc247d554a3
BrightAnna <AnyRise@mail.ru>
2024-12-03 00:20:43 +0300

    initial commit

--- selenide-project/src/test/java/TestBase.java
+++ selenide-project/src/test/java/TestBase.java
@@ -1,10 +1,19 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
 import com.epam.reportportal.testng.ReportPortalTestNGListener;
+import io.qameta.allure.Allure;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.Platform;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 import static com.codeborne.selenide.Browsers.CHROME;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
@@ -35,7 +44,16 @@ public class TestBase {
     }
 
     @AfterMethod
-    public void methodTearDown() {
+    public void methodTearDown(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            File screenshot = ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.FILE);
+            try {
+                Allure.addAttachment("screenshot" + result.getMethod().getMethodName(), new FileInputStream(screenshot));
+            } catch (FileNotFoundException e) {
+                System.out.println("Couldn't take screenshot\n" + e.getMessage());
+            }
+        }
+
         closeWebDriver();
     }
 }
